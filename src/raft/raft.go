d0bfb79f179a1b2ee2b072e4b92d10b2e6c3a066
bfforever <1640528278@qq.com>
2022-07-09 15:20:51 +0800

    test 分支

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -272,7 +272,7 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.me = me
 
 	// Your initialization code here (2A, 2B, 2C).
-
+	code
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
 
